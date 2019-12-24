07ee87e84690b1675d02cd93a395ca721c994ff3
DevJaye <devjaye@163.com>
2019-12-24 15:18:01 +0800

    学习新的IDE(idea) 复习知识到方法重载

---
+++ Java/01_Java_base/Day_03/src/com/dj/day04/demo01/Sequence.java
@@ -0,0 +1,12 @@
+package com.dj.day04.demo01;
+
+public class Sequence {
+    public static void main(String[] args) {
+        System.out.println("01");
+        System.out.println("02");
+        System.out.println("03");
+        System.out.println("04");
+    }
+
+
+}
---
+++ Java/01_Java_base/Day_03/src/com/dj/day04/demo02/If.java
@@ -0,0 +1,15 @@
+package com.dj.day04.demo02;
+
+public class If {
+    public static void main(String[] args) {
+        System.out.println("今天走在街上想去打网吧");
+        int age = 18;
+        if (age>=18) {
+            System.out.println("进入网吧");
+            System.out.println("开始开黑");
+            System.out.println("遇到猪队友");
+            System.out.println("下机");
+        }
+        System.out.println("回家吃饭");
+    }
+}
